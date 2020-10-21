3dab1abbd9d71f732f39d76d7c897b7b25137837
daniyarka <daniyarkurmanbayev123@gmail.com>
2020-10-21 12:47:26 +0600

    static urls changed

--- goalster/settings.py
+++ goalster/settings.py
@@ -128,12 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATIC_URL = "/api/staticfiles/"
+STATIC_URL = "/staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_URL = '/api/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CELERY_BROKER_URL = os.environ.get('BROKER_URL', 'amqp://localhost')
